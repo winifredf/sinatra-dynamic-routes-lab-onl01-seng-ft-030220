6980c5619311087b65720f0fb0f75631dbe42019
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-16 23:11:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,9 +34,12 @@ class App < Sinatra::Base
   
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
-    
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "add"
+      "#{@number1} + #{@number2}"
+    elsif 
+  end
     
     # Write your code here!
 
