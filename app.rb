8a2b981ef9dfa131506c5bb2f5f229564678c3e8
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-16 22:44:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ class App < Sinatra::Base
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
-    
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{"
   # Write your code here!
 
 end
