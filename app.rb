39911113554d39bd091744e13d08f652293b13f7
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-16 22:35:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get "/reversename/:name" do
-    @user_name = params[:name.reverse]
+    @user_name = params[:name]
   end
   # Write your code here!
 
