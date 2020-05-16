485e97ce702c2a87636249c20ae9d06f168ca6a1
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-16 22:54:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
   
   get "/say/:number/:phrase" do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
     phrase = ""
     @number.times do
